c495c67cc0583afbef2de2c9c552872633e4d782
Riften <huangyongxi@sjtu.edu.cn>
2018-06-13 14:07:01 +0800

    change text

--- pylib/dataLoader.py
+++ pylib/dataLoader.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch.utils.data as Data
+import torch
 
 class npLoader():
     def __init__(self):
@@ -17,3 +19,26 @@ class npLoader():
     def loadLabel(self, labelPath, train = True):
         label = np.fromfile(labelPath, dtype = np.uint8)
         return label
+
+class My_MNIST(Data.Dataset):
+    def __init__(self, data_path, label_path, train=True, transform=None, target_transform=None, download=False):
+        self.transform = transform
+        self.target_transform = target_transform
+        self.train = train  # training set or test set
+        self.test_num = 10000
+        self.train_num = 60000
+        if train:
+            self.data = torch.Tensor(np.fromfile(data_path,dtype=np.uint8).reshape(60000,1,45,45))/255
+        else:
+            self.data = torch.Tensor(np.fromfile(data_path,dtype=np.uint8).reshape(10000,1,45,45))/255
+        self.label = torch.LongTensor(np.fromfile(label_path,dtype=np.uint8))
+        
+ 
+    def __getitem__(self, index):
+        return self.data[index], self.label[index]
+ 
+    def __len__(self):
+        if self.train:
+            return 60000
+        else:
+            return 10000
